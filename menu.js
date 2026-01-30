db45bcdac685fb41f193788141356bfa878fb4b4
sahipushpak <4waygaming12@gmail.com>
2026-01-30 09:14:03 +0530

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,87 @@
+const menuData = {
+  "Shuruaat": [
+    { name: "South Indian Papad (Aplam)", price: 25 },
+    { name: "Rasam + Aplam Small", price: 60 },
+    { name: "Rasam + Aplam", price: 100 },
+    { name: "Upma", price: 120 }
+  ],
+
+  "Soup": [
+    { name: "Cream of Tomato Soup", price: 100 },
+    { name: "Kali Mirch Soup", price: 100 },
+    { name: "Vegetable Clear Soup", price: 100 },
+    { name: "Sweet Corn Soup", price: 110 },
+    { name: "Hot & Sour Soup", price: 110 },
+    { name: "Manchow Soup", price: 110 },
+    { name: "Lemon Coriander Soup", price: 110 }
+  ],
+
+  "Idli & Vada": [
+    { name: "Rice Idli (2 pcs)", price: 60 },
+    { name: "Medu Vada (4 pcs)", price: 160 },
+    { name: "Rava Idli", price: 160 },
+    { name: "Dahi Idli", price: 160 },
+    { name: "Dahi Vada (1 pc)", price: 70 },
+    { name: "Idli Chaat", price: 160 },
+    { name: "Spicy Mini Idli", price: 150 }
+  ],
+
+  "Classic Dosa": [
+    { name: "Plain Dosa", price: 109 },
+    { name: "Masala Dosa", price: 139 },
+    { name: "Paper Plain Dosa", price: 149 },
+    { name: "Mysore Masala Dosa", price: 149 },
+    { name: "Butter Masala Dosa", price: 159 },
+    { name: "Paneer Masala Dosa", price: 149 },
+    { name: "Karnataka Special Masala Dosa", price: 169 }
+  ],
+
+  "Fusion Dosa": [
+    { name: "Spring Roll Dosa", price: 199 },
+    { name: "Schezwan Masala Dosa", price: 199 },
+    { name: "Butter Masala Cheese Dosa", price: 199 },
+    { name: "Schezwan Paneer Dosa", price: 209 },
+    { name: "Vegetable Cheese Dosa", price: 209 }
+  ],
+
+  "Uttapam": [
+    { name: "Plain Uttapam", price: 149 },
+    { name: "Onion Uttapam", price: 169 },
+    { name: "Tomato Uttapam", price: 169 },
+    { name: "Mix Veg Uttapam", price: 179 },
+    { name: "Paneer Uttapam", price: 199 }
+  ],
+
+  "South Indian Rice": [
+    { name: "Sambar Rice", price: 159 },
+    { name: "Lemon Rice", price: 159 },
+    { name: "Curd Rice", price: 159 },
+    { name: "South Indian Veg Biryani", price: 189 }
+  ],
+
+  "Chinese Starters": [
+    { name: "Spring Roll Veg", price: 129 },
+    { name: "Veg Manchurian Dry", price: 179 },
+    { name: "Chilli Paneer Dry", price: 229 }
+  ],
+
+  "North Indian": [
+    { name: "Paneer Butter Masala", price: 279 },
+    { name: "Shahi Paneer", price: 279 },
+    { name: "Dal Makhani", price: 199 },
+    { name: "Jeera Aloo", price: 199 }
+  ],
+
+  "Beverages": [
+    { name: "Tea / Masala Tea", price: 40 },
+    { name: "Filter Coffee", price: 70 },
+    { name: "Cold Coffee", price: 99 },
+    { name: "Mint Mojito", price: 109 }
+  ],
+
+  "Dessert": [
+    { name: "Gulab Jamun (2 pcs)", price: 60 },
+    { name: "Rava Kesari", price: 105 },
+    { name: "Ice Cream Double Scoop", price: 50 }
+  ]
+};
